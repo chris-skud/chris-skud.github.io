ea01d33e8929e05e4b1274617955367f2b9527c5
Chris Skudlarczyk <chris.skud@gmail.com>
2023-06-10 14:30:47 -0500

    some cleanup and max url size handling

--- stageplot2/scripts/main.js
+++ stageplot2/scripts/main.js
@@ -1,5 +1,5 @@
-const baseUrl = "https://chris-skud.github.io/stageplot2";
-// const baseUrl = "http://localhost:9000";
+// const baseUrl = "https://chris-skud.github.io/stageplot2";
+const baseUrl = "http://localhost:9000";
 let canvas = null;
 const deleteIcon = "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Ccircle style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:white;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:white;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E";
 const deleteImg = document.createElement('img');
@@ -10,7 +10,6 @@ const init = () => {
   window.addEventListener('resize', resizeCanvas, false);
 
   function resizeCanvas() {
-      // canvas.setHeight(window.innerHeight);
       canvas.setWidth(window.innerWidth);
       canvas.renderAll();
   }
@@ -63,7 +62,6 @@ const renderIcon = (icon) => {
 const setLocalStorage = () => {
   const state = getPageState();
   localStorage.setItem("state", JSON.stringify(state));
-  console.log(state);
 }
 
 const loadData = () => {
@@ -75,11 +73,15 @@ const loadData = () => {
       }
   } else {
     state = JSON.parse(LZString.decompressFromEncodedURIComponent(u.searchParams.get("s")));
-    console.log(`geting from url: ${JSON.stringify(state)}`);
   }
   
+  // clear the session param so there's no confusion about where the data is coming from
+  // as once pulled out of session, it's in local storage.
   window.history.replaceState('', 'Stageplot', baseUrl);
 
+  if (state == null) {
+    return;
+  }
   document.getElementById("name").value = state.name;
   document.getElementById("venue").value = state.venue;
   document.getElementById("date").value = state.date;
@@ -94,13 +96,14 @@ const loadData = () => {
 }
 
 const share = () => {
-  console.log(`share state: ${JSON.stringify(getPageState())}`)
   const state = LZString.compressToEncodedURIComponent(JSON.stringify(getPageState()));
-  console.log("Size of compressed sample is: " + state.length);
-  // const state = btoa(JSON.stringify(getPageState()));
-  // console.log("otherwise " + state.length);
-  console.log(state);
   navigator.clipboard.writeText(baseUrl+"?s="+state);
+  // The whole session in the URL is bunch of crap to enable hosting on a static site.
+  if ((state.length + baseUrl) > 8201) { // 8201 is max url length supported by github pages
+    alert("Hey there awesome person!\n\nThe link we create for you includes all the content from the page which makes sharing it really easy. Unfortunately, there's a limit to how much content the links can support and it appears your big beautiful band is just too much. Your best option is probably going to be to print to PDF through your browser and share that instead.")
+  } else {
+    alert("A link to your stageplot has been copied the clipboard. The link itself includes the content for the page so be sure to click it again if you want to share any subsequent updates.");
+  }
 }
 
 const getPageState = () => {
